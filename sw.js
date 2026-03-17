a794965fc30481c8cd612f615e81a767470c7e9c
Lollipop-yun <2404025136@qq.com>
2026-03-17 23:28:01 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,20 @@
-const CACHE_NAME = 'lindongji-v1';
+const CACHE_NAME = 'winter-os-v1';
 const urlsToCache =[
-  './',
-  './index.html',
-  './storage.js',
-  './manifest.json'
+    './index.html',
+    './storage.js',
+    './manifest.json'
 ];
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(cache => cache.addAll(urlsToCache))
+    );
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
+    event.respondWith(
+        caches.match(event.request)
+        .then(response => response || fetch(event.request))
+    );
 });
